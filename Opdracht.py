2f0d8e971329696d3811980b805eb1e3b95946ac
Lars Versteeg <lars.versteeg1999@gmail.com>
2020-05-06 13:40:40 +0200

    Added many operators and updated the readme

--- Opdracht.py
+++ Opdracht.py
@@ -1,30 +1,19 @@
 from operators import *
 from nodes import *
-from typing import Callable
+from typing import Callable, Union
 
 Program_state = {"R0": 0, "R1": 0, "R2": 0, "R3": 0, "R4": 0, "R5": 0, "R6": 0, "R7": 0, "R8": 0, "R9": 0, "R10": 0, "R11": 0, "R12": 0, "R13": 0, "R14": 0, "PC": 0, }
 
+#This function reads the programfile and returns a list of strings(one per line)
 def Read_file_into_lines(filename) -> [str]:
     file = open(filename, "r")
     return file.read().splitlines()
 
+#This funtion turns a list of strings into a list of operator_node
 def Lines_into_operations(Lines : [str]) -> [operator_node]:
     return list(map(Line_to_operation, Lines))
 
-def Check_reg(name : str):# -> Union[int, str]:
-    if(name.isdigit()):
-        return int(name)
-    elif (name[0] == '-' and name[1:].isdigit()):
-        return int(name)
-    else:
-        return name
-        
-def Check_reg2(name : str, Program_state : dict):# -> Union[int, str]:
-    if(type(name) == int):
-        return name
-    else:
-        return Program_state.get(name)
-
+#This function turns a single line into a operator_node
 def Line_to_operation(line : str) -> operator_node:
     line = line.split()
     if line[0] == "\n":
@@ -35,10 +24,28 @@ def Line_to_operation(line : str) -> operator_node:
         return operator_node(operators.SUB, line[1], Check_reg(line[2]), Check_reg(line[3]))
     elif line[0] == "ANTMAN":
         return operator_node(operators.MUL, line[1], Check_reg(line[2]), Check_reg(line[3]))
-    elif line[0] == "CAPTAINAMERICA":
-        return operator_node(operators.ADD, line[1], Check_reg(line[2]), 0)
     elif line[0] == "HULK":
         return operator_node(operators.DIV, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "DOCTORSTRANGE":
+        return operator_node(operators.POW, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "DIEDERIK":
+        return operator_node(operators.MOD, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "NICKFURY":
+        return operator_node(operators.FDIV, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "KAECILIUS":
+        return operator_node(operators.EQ, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "REDSKULL":
+        return operator_node(operators.NEQ, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "ULTRON":
+        return operator_node(operators.GRE, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "MANDARIN":
+        return operator_node(operators.GREEQ, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "RONAN":
+        return operator_node(operators.LES, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "MYSTERIO":
+        return operator_node(operators.LESEQ, line[1], Check_reg(line[2]), Check_reg(line[3]))
+    elif line[0] == "CAPTAINAMERICA":
+        return operator_node(operators.ADD, line[1], Check_reg(line[2]), 0)
     elif line[0] == "BLACKWIDOW":
         return jmp_node(operators.JMPT, line[1], line[2])
     elif line[0] == "HAWKEYE":
@@ -56,22 +63,41 @@ def Line_to_operation(line : str) -> operator_node:
     else:
         return None
 
+#This function checks if a string is a digit or not
+def Check_reg(name : str) -> Union[str, int]:
+    if(name.isdigit()):
+        return int(name)
+    elif (name[0] == '-' and name[1:].isdigit()):
+        return int(name)
+    else:
+        return name
+
+#This function checks if the variable already exists or not
+#If it already exists, it returns its value       
+def Check_existing(name : str, Program_state : dict) -> int:
+    if(type(name) == int):
+        return name
+    else:
+        return Program_state.get(name)
+
+#This function executes a node by calling its operator
 def execute(operation_node : node, Program_state : dict, program : [operator_node]) -> dict:
     if (isinstance(operation_node, operator_node)):
-        return {operation_node.storage_register : operation_node.operator.value(Check_reg2(operation_node.parameter1, Program_state), Check_reg2(operation_node.parameter2, Program_state))}
+        return {operation_node.storage_register : operation_node.operator.value(Check_existing(operation_node.parameter1, Program_state), Check_existing(operation_node.parameter2, Program_state))}
     elif (isinstance(operation_node, jmp_node)):
-        return operation_node.operator.value(Check_reg2(operation_node.condition, Program_state), operation_node.jmp_location)
+        return operation_node.operator.value(Check_existing(operation_node.condition, Program_state), operation_node.jmp_location)
     elif (isinstance(operation_node, jmp_conditional_node)):
-        condition = Check_reg2(operation_node.parameter1, Program_state) == Check_reg2(operation_node.parameter2, Program_state)
+        condition = Check_existing(operation_node.parameter1, Program_state) == Check_existing(operation_node.parameter2, Program_state)
         return operation_node.operator.value(condition, operation_node.jmp_location)
     elif (isinstance(operation_node, jmp_label_node)):
         return operation_node.operator.value(operation_node.name, program)
     elif (isinstance(operation_node, print_node)):
-        operation_node.operator.value(Check_reg2(operation_node.value, Program_state))
+        operation_node.operator.value(Check_existing(operation_node.value, Program_state))
         return {}
     else:      
         return {}
 
+#This function executes a complete program(list of nodes)
 def run(Program_state : dict, program : [operator_node]) -> Program_state:
     New_Program_state = Program_state.copy()
     if len(program) <= 1:
@@ -88,6 +114,8 @@ def run(Program_state : dict, program : [operator_node]) -> Program_state:
     New_Program_state.update({"PC": New_Program_state.get("PC") + 1})   
     return run(New_Program_state, program)
 
+#This is the decorator for the read_file_into_lines function
+#It enables the function to print
 def Printing_read_file_into_lines(func: Callable) -> [str]:
     def inner(Filename):
         output = func(Filename)
@@ -96,6 +124,8 @@ def Printing_read_file_into_lines(func: Callable) -> [str]:
         return output
     return inner
 
+#This is the decorator for the lines_into_operations function
+#It enables the function to print
 def Printing_lines_into_operations(func: Callable) -> [operator_node]:
     def inner(Lines: [str]):
         output = func(Lines)
@@ -104,15 +134,17 @@ def Printing_lines_into_operations(func: Callable) -> [operator_node]:
         return output
     return inner
 
-def all_steps(Filename, Program_state : dict, Printing_state = "n") -> Program_state:
+#This is the main function
+#It contains the lexing, parsing and execution steps
+def all_steps(Filename, Program_state : dict, Printing_state = 0) -> Program_state:
     global Read_file_into_lines
     global Lines_into_operations 
 
-    if Printing_state == "y":
+    if Printing_state:
         Read_file_into_lines = Printing_read_file_into_lines(Read_file_into_lines)
         Lines_into_operations = Printing_lines_into_operations(Lines_into_operations)
 
     return run(Program_state, Lines_into_operations(Read_file_into_lines(Filename)))
 
-output = all_steps("countermachine.txt", Program_state,"n")
+output = all_steps("countermachine.txt", Program_state, 0)
 print("Program_output: ", output)
--- README.md
+++ README.md
@@ -4,21 +4,92 @@ Like they say in dutch "Een beetje van jezelf en een beetje van maggi.".
 
 ## Variables
 Like assembler, this language has 16 base registers, the 16th(R15) being used for the Program Counter.<br>
-But unlike traditional assembler, AVENGERS ASSEMBLER! gives you the ability to declare your own variables.
+But unlike traditional assembler, AVENGERS ASSEMBLER! gives you the ability to declare your own variables. You can simply do this by using IRONMAN, THOR, ANTMAN, HULK OR CAPTAINAMERICA use a new name as the storage variable.
 
 ## List of instructions
 
-| Name            | Instruction              | Example  | Explanation
-| ------------- |:-------------:| ------------:|--------:|
-| IRONMAN           | ADDITION          | IRONMAN S P1 P2 | Add P1 and P2 and store it in S
-| THOR              | SUBTRACTION       | THOR S P1 P2 | Subtract P2 from P1 and store it in S
-| ANTMAN            | MULTIPLICATION    | ANTMAN S P1 P2 | Multiply P1 and P2 and store it in S
-| HULK              | DIVISION          | HULK S P1 P2 | Divide P1 by P2 and store it in S
-| CAPTAINAMERICA    | STORE             | CAPTAINAMERICA S P1 | Stores P1 in S
-| BLACKWIDOW        | JUMP TRUE         | BLACKWIDOW P1 R | If P1 is true, jump to row rumber R
-| HAWKEYE           | JUMP FALSE        | HAWKEYE P1 R | If P1 is false, jump to row rumber R
-| VISION            | PRINT             | VISION P1 | Print P1
-| SCARLETWITCH      | JUMP EQUAL        | SCARLETWITCH P1 P2 R | If P1 equals P2, jump to row rumber R
-| FALCON            | JUMP NOT EQUAL    | FALCON P1 P2 R | If P1 does not equal P2, jump to row rumber R |
-| SPIDERMAN         | MAKE LABEL        | SPIDERMAN NAME | Makes a label called NAME
-| WARMACHINE        | JUMP LABEL        | WARMACHINE NAME | Jump to a label called NAME
+| Name              | Instruction       | Example               | Explanation
+| -------------     |:--------          |:------------          |:--------|
+| IRONMAN           | ADDITION          | IRONMAN S P1 P2       | Add P1 and P2 and store it in S
+| THOR              | SUBTRACTION       | THOR S P1 P2          | Subtract P2 from P1 and store it in S
+| ANTMAN            | MULTIPLICATION    | ANTMAN S P1 P2        | Multiply P1 and P2 and store it in S
+| HULK              | DIVISION          | HULK S P1 P2          | Divide P1 by P2 and store it in S
+| DOCTORSTRANGE     | POWER             | DOCTORSTRANGE S P1 P2 | Raise P1 to the power of P2 and store it in S
+| DIEDERIK          | MODULUS           | DIEDERIK S P1 P2      | Divide P1 by P2 and store the remainder in S
+| NICKFURY          | FLOOR DIVISION    | NICKFURY S P1 P2      | Divide P1 by P2 and the value before the point in S
+| KAECILIUS         | EQUALS            | KAECILIUS S P1 P2     | Store P1 == P2 in S
+| REDSKULL          | NOT EQUALS        | REDSKULL S P1 P2      | Store P1 != P2 in S
+| ULTRON            | GREATER           | ULTRON S P1 P2        | Store P1 > P2 in S
+| MANDARIN          | EQUALS OR EQUALS  | MANDARIN S P1 P2      | Store P1 >= P2 in S
+| RONAN             | LESSER            | RONAN S P1 P2         | Store P1 < P2 in S
+| MYSTERIO          | LESSER OR EQUALS  | MYSTERIO S P1 P2      | Store P1 <= P2 in S
+| CAPTAINAMERICA    | STORE             | CAPTAINAMERICA S P1   | Stores P1 in S
+| BLACKWIDOW        | JUMP TRUE         | BLACKWIDOW P1 R       | If P1 is true, jump to row rumber R
+| HAWKEYE           | JUMP FALSE        | HAWKEYE P1 R          | If P1 is false, jump to row rumber R
+| VISION            | PRINT             | VISION P1             | Print P1
+| SCARLETWITCH      | JUMP EQUAL        | SCARLETWITCH P1 P2 R  | If P1 equals P2, jump to row rumber R
+| FALCON            | JUMP NOT EQUAL    | FALCON P1 P2 R        | If P1 does not equal P2, jump to row rumber R |
+| SPIDERMAN         | MAKE LABEL        | SPIDERMAN NAME        | Makes a label called NAME
+| WARMACHINE        | JUMP LABEL        | WARMACHINE NAME       | Jump to a label called NAME
+
+###### My favorite is the modulus operator :P
+
+## Example
+Here is an example of the minsky countermachine: <br>
+```
+CAPTAINAMERICA R1 0
+IRONMAN R1 R1 1
+THOR R1 R1 1
+CAPTAINAMERICA R2 R1
+HAWKEYE R1 7
+CAPTAINAMERICA R3 10
+SCARLETWITCH R1 R2 9
+CAPTAINAMERICA R4 10
+CAPTAINAMERICA R5 10
+```
+
+## How to run
+All you have to do to run your code is: 
+
+```
+output = all_steps("countermachine.txt", Program_state, 1)
+print("Program_output: ", output)
+```
+
+Here you can change the textfile you want to run, in this case it is countermachine.txt. The last variable is whether you want printing mode turned on or off.<br>
+
+Printing mode prints the raw output of the lexing and parsing steps, aswell as the regular output.<br>
+
+The output of the code above is as follows with printing mode off:
+
+```
+Program_output:  {'R0': 0, 'R1': 0, 'R2': 0, 'R3': 0, 'R4': 0, 'R5': 10, 'R6': 0, 'R7': 0, 'R8': 0, 'R9': 0, 'R10': 0, 'R11': 0, 'R12': 0, 'R13': 0, 'R14': 0, 'PC': 9}
+```
+
+And with printing mode turned on:
+
+```
+Lines: 
+['CAPTAINAMERICA R1 0', 'IRONMAN R1 R1 1', 'THOR R1 R1 1', 'CAPTAINAMERICA R2 R1', 'HAWKEYE R1 7', 'CAPTAINAMERICA R3 10', 'SCARLETWITCH R1 R2 9', 'CAPTAINAMERICA R4 10', 'CAPTAINAMERICA R5 10']
+Nodes: 
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R1 Parameter1: 0 Parameter2: 0
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R1 Parameter1: R1 Parameter2: 1
+<class 'nodes.operator_node'> Operator: SUBTRACT Storage_register: R1 Parameter1: R1 Parameter2: 1
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R2 Parameter1: R1 Parameter2: 0
+<class 'nodes.jmp_node'> Operator: JUMP_FALSE Condition: R1 jmp_location: 7
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R3 Parameter1: 10 Parameter2: 0
+<class 'nodes.jmp_conditional_node'> Operator: JUMP_TRUE Jmp_location: 9 Parameter1: R1 Parameter2: R2
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R4 Parameter1: 10 Parameter2: 0
+<class 'nodes.operator_node'> Operator: ADDITION Storage_register: R5 Parameter1: 10 Parameter2: 0
+Program_output:  {'R0': 0, 'R1': 0, 'R2': 0, 'R3': 0, 'R4': 0, 'R5': 10, 'R6': 0, 'R7': 0, 'R8': 0, 'R9': 0, 'R10': 0, 'R11': 0, 'R12': 0, 'R13': 0, 'R14': 0, 'PC': 9}
+```
+
+
+
+
+
+
+
+
+
+
